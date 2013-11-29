7268d09f3e160d82c5ae9abb7b4ad40388179e02
Karun Viswanath <karunv@gmail.com>
2013-11-29 10:00:55 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InnowowApp::Application.config.secret_key_base = '79d4d7322c5012b151971deac35faf39ce126000a5ce710402da3955c2606f96d27edb2ae10e0268365c6139d7c42d06e34520127543f529c58efecdc84bf2c7'
