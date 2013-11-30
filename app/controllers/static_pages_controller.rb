4e337f9f319cc0845a0bcdc91c9fc438ab460ca2
Karun Viswanath <karunv@gmail.com>
2013-11-30 00:23:28 -0800

    added static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def careers
+  end
+
+  def support
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 InnowowApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/services"
+  get "static_pages/careers"
+  get "static_pages/support"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
